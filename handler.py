bd2baf1fa8173f8c75df4fe2421c4c29c4977da2
blotero <blotero@gmail.com>
2021-06-12 10:08:13 -0500

    New functions. Cleaner patient handling.

---
+++ handler.py
@@ -0,0 +1,249 @@
+import os
+import numpy as np
+from matplotlib import pyplot
+from PIL import Image
+from matplotlib.path import Path
+from scipy.interpolate import interp1d
+import json
+import xml.etree.ElementTree as ET
+import logging
+
+
+annotations_path = os.path.join('database', "Annotations", "users", "lauralrh")
+images_path = os.path.join('database', "Images", "users", "lauralrh")
+
+
+def name_r():
+    return str(np.random.random(1)[0])[2:]
+
+
+def fix_right_left(data):
+    w = list(data.keys())
+    for key in w:
+        ks = list(data[key].keys())[:]
+        # print(ks)
+
+        if len(ks) > 1:
+
+            data[key]['right'] = data[key][str(min(list(map(int, ks))))]
+            data[key]['left'] = data[key][str(max(list(map(int, ks))))]
+
+            del data[key][str(min(list(map(int, ks))))]
+            del data[key][str(max(list(map(int, ks))))]
+
+        else:
+            if int(ks[0]) > 200:
+                data[key]['left'] = data[key][str(min(list(map(int, ks))))]
+            else:
+
+                data[key]['right'] = data[key][str(min(list(map(int, ks))))]
+
+    return data
+
+
+# def get_polygons(image, annotation, range_):
+
+#     logging.info(f"Loadding: {annotation}")
+#     tree = ET.parse(annotation)
+#     root = tree.getroot()
+
+#     polygons = {}
+
+#     for obj in root.findall('object'):
+
+#         name = obj.find('name').text
+#         id_ = obj.find('id').text
+
+#         if int(id_) < len( root.findall('object'))//2:
+#             id_ = 'right'
+#         else:
+#             id_ = 'left'
+
+#         polygon = []
+#         for pt in obj.find('polygon').findall('pt'):
+#             polygon.append([pt.find('x').text, pt.find('y').text])
+
+#         if not name in polygons:
+#             polygons[name] = {}
+
+#         polygons[name][id_] = polygon
+
+
+#     logging.info(f"Loadding: {image}")
+#     img = np.array(Image.open(image))[:,:,0]
+#     # pyplot.figure(figsize=(20, 10), dpi=90)
+#     # pyplot.imshow(img)
+#     scale_min, scale_max = range_#[os.path.split(image)[-1]]
+
+#     logging.info(f"scalling from {scale_min}-{scale_max} to 0-255")
+
+#     polygons_arr = {}
+#     mask = np.array(list(zip(*np.mgrid[0:640, 0:480].reshape(2, -1))))
+#     for name in polygons:
+#         for id_ in polygons[name]:
+
+#     #         line = pyplot.Polygon(polygons[name][id_], closed=True, color=f"C{name}", alpha=1, fill=True)
+#     #         pyplot.gca().add_line(line)
+
+#             p = Path(polygons[name][id_])
+#             grid = p.contains_points(mask)
+
+#             if not name in polygons_arr:
+#                 polygons_arr[name] = {}
+
+#             adjust_scale = interp1d([0, 255], [scale_min, scale_max])
+
+#             polygons_arr[name][id_] = adjust_scale(np.array([img.T.item(tuple(p)) for p in mask[grid]]))
+
+#     logging.info(f"{len(polygons_arr)} polygons")
+#     logging.info(f"IDs: ", {k:len(polygons_arr[k]) for k in polygons_arr.keys()})
+
+#     logging.info("-"*70)
+
+#     return polygons_arr
+
+
+def get_polygons(image, annotation, range_):
+
+    logging.info(f"Loadding: {annotation}")
+    tree = ET.parse(annotation)
+    root = tree.getroot()
+
+    polygons = {}
+
+    for obj in root.findall('object'):
+
+        name = obj.find('name').text
+        id_ = obj.find('id').text
+
+#         if int(id_) < len( root.findall('object'))//2:
+#             id_ = 'right'
+#         else:
+#             id_ = 'left'
+
+        id_ = name_r()
+
+        polygon = []
+        for pt in obj.find('polygon').findall('pt'):
+            polygon.append([pt.find('x').text, pt.find('y').text])
+
+
+#         print(pt.find('x').text)
+        id_ = pt.find('x').text
+
+#         if int(id_) < len( root.findall('object'))//2:
+#             id_ = 'right'
+#         else:
+#             id_ = 'left'
+
+        if not name in polygons:
+            polygons[name] = {}
+
+        polygons[name][id_] = polygon
+
+    logging.info(f"Loadding: {image}")
+    img = np.array(Image.open(image))[:, :, 0]
+    # pyplot.figure(figsize=(20, 10), dpi=90)
+    # pyplot.imshow(img)
+    scale_min, scale_max = range_  # [os.path.split(image)[-1]]
+
+    logging.info(f"scalling from {scale_min}-{scale_max} to 0-255")
+
+    polygons_arr = {}
+    mask = np.array(list(zip(*np.mgrid[0:640, 0:480].reshape(2, -1))))
+    for name in polygons:
+        for id_ in polygons[name]:
+
+    #         line = pyplot.Polygon(polygons[name][id_], closed=True, color=f"C{name}", alpha=1, fill=True)
+    #         pyplot.gca().add_line(line)
+
+            p = Path(polygons[name][id_])
+            grid = p.contains_points(mask)
+
+            if not name in polygons_arr:
+                polygons_arr[name] = {}
+
+            adjust_scale = interp1d([0, 255], [scale_min, scale_max])
+
+            polygons_arr[name][id_] = adjust_scale(
+                np.array([img.T.item(tuple(p)) for p in mask[grid]]))
+
+    logging.info(f"{len(polygons_arr)} polygons")
+    logging.info(f"IDs: ", {k: len(polygons_arr[k])
+                            for k in polygons_arr.keys()})
+
+    logging.info("-" * 70)
+
+#     return polygons_arr
+    return fix_right_left(polygons_arr)
+
+
+def get_temperatures(patient, range_):
+
+    poly = []
+
+    file_range = False
+    if isinstance(range_, str):
+        ranges = image = os.path.join(images_path, patient, f'{range_}.json')
+        ranges = json.load(open(ranges, 'r'))
+        file_range = True
+
+    for file in os.listdir(os.path.join(images_path, patient)):
+        if not file.endswith('.jpg'):
+            continue
+
+        image = os.path.join(images_path, patient, file)
+        annotation = os.path.join(
+            annotations_path, patient, file.replace('.jpg', '.xml'))
+
+        if file_range:
+            range_ = ranges[file]
+
+        poly.append([file, get_polygons(image, annotation, range_)])
+
+    return poly
+
+
+def show_temperatures(patient, fn='mean', range_='scale', sufix='', fig=None):
+
+    if fig is None:
+        fig_ = pyplot.figure(figsize=(20, 15), dpi=90)
+
+    poly = get_temperatures(patient, range_)
+    pyplot.subplot(2, 3, 1)
+    pyplot.title(f"{patient}{sufix}")
+    pyplot.imshow(Image.open(os.path.join('assets', 'figlabels.png')))
+    pyplot.axis('off')
+    Position = ['Plantar medial', 'Safeno',
+                'Tibial', 'Sural', 'Plantar lateral']
+    labels = ['L.4.5', 'L.3.4', 'S.1.2', 'S.1.2', 'S.1.2']
+    for i in range(1, 6):
+        P = str(i)
+
+        r, l, t = [], [], []
+        for t_, p in poly:
+
+            if not P in p:
+                continue
+
+            t = int(t_.replace('.jpg', '').replace('t', ''))
+
+            if 'right' in p[P]:
+                r.append((t, getattr(np, fn)(p[P]['right'])))
+            if 'left' in p[P]:
+                l.append((t, getattr(np, fn)(p[P]['left'])))
+
+        pyplot.subplot(2, 3, i + 1)
+        pyplot.title(f"{Position[i-1]}: {labels[i-1]}")
+        pyplot.plot(*list(zip(*sorted(r))), '-o', label='right', c='C0')
+        pyplot.plot(*list(zip(*sorted(l))), '-o', label='left', c='C1')
+        if fig is None:
+            pyplot.legend()
+        pyplot.ylabel('Temperature [°C]')
+        pyplot.xlabel('Time [$s$]')
+        pyplot.grid(True)
+
+    if fig is None:
+
+        fig_.savefig(os.path.join('out', f"{patient}{sufix}.png"))
+        pyplot.show()
--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from pathlib import Path
 import sys
 import matplotlib.pyplot as plt
 import numpy as np
-
+import cv2
 from PySide2.QtWidgets import QApplication, QMainWindow, QGraphicsView, QLabel, QPushButton, QTextBrowser, QAction, QFileDialog, QDialog, QDialog
 from PySide2.QtCore import QFile, QObject, SIGNAL, QDir 
 from PySide2.QtUiTools import QUiLoader 
@@ -17,7 +17,6 @@ from PySide2.QtGui import QImage, QPixmap
 from segment import Image_2_seg
 from manualseg import manualSeg
 
-import cv2
 
 class Window(QMainWindow):
     def __init__(self):
@@ -31,6 +30,14 @@ class Window(QMainWindow):
         self.inputExists = False
         self.defaultDirectoryExists = False
 
+    def load_ui(self):
+        loader = QUiLoader()        
+        path = os.fspath(Path(__file__).resolve().parent / "form.ui")
+        ui_file = QFile(path)
+        ui_file.open(QFile.ReadOnly)
+        self.ui_window = loader.load(ui_file, self)
+        ui_file.close()
+    
     def make_connect(self):
         QObject.connect(self.ui_window.segButton, SIGNAL ('clicked()'), self.segment)
         QObject.connect(self.ui_window.tempButton, SIGNAL ('clicked()'), self.temp_extract)
@@ -42,19 +49,15 @@ class Window(QMainWindow):
         QObject.connect(self.ui_window.previousImageButton , SIGNAL ('clicked()'), self.previousImage)
         QObject.connect(self.ui_window.saveButton , SIGNAL ('clicked()'), self.saveImage)
 
-    def load_ui(self):
-        loader = QUiLoader()        
-        path = os.fspath(Path(__file__).resolve().parent / "form.ui")
-        ui_file = QFile(path)
-        ui_file.open(QFile.ReadOnly)
-        self.ui_window = loader.load(ui_file, self)
-        ui_file.close()
-
-    def messagePrint(self):
+    def messagePrint(self, message):
         #INPUT: string to print
         #OUTPUT: none
-        #ACTION. generate out.html file and refresh it in Messages QTextArea
-        pass
+        #ACTION: generate out.html file and refresh it in Messages QTextArea
+        out_file = open("out.html" , "w")
+        out_file.write(message)
+        out_file.close()
+        self.ui_window.textBrowser.setSource('out.html')
+        self.ui_window.textBrowser.reload()
 
     def findImages(self):
         self.fileList = []
@@ -83,24 +86,19 @@ class Window(QMainWindow):
         self.i2s = Image_2_seg()
         self.i2s.setPath(self.opdir)
         self.i2s.extract()
-        print(self.i2s.Y_pred)
         threshold =  0.5
         img = plt.imread(self.opdir)/255
-        Y = self.i2s.Y_pred / self.i2s.Y_pred.max()
+        Y = self.i2s.Y_pred
+        Y = Y / Y.max()
         Y = np.where( Y >= threshold  , 1 , 0)
-        Y = cv2.resize(Y[0] , (self.inputImage.shape[1] , self.inputImage.shape[0] ) , interpolation = cv2.INTER_NEAREST)
-        Y = cv2.cvtColor(Y , cv2.COLOR_BGR2RGB)
-        cv2.imwrite("output.jpg" , Y)
+        Y = cv2.resize(Y[0], (img.shape[1],img.shape[0]), interpolation = cv2.INTER_NEAREST) # Resize the prediction to have the same dimensions as the input 
+        #Y_im = cv2.cvtColor(Y, cv2.COLOR_BGR2RGB)
+        #cv2.imwrite("output.jpg", Y_im) 
+        plt.imsave("output.jpg" , Y*0.5+img[:,:,0] , cmap='gray')
         self.ui_window.outputImg.setPixmap("output.jpg")
 
-
     def segment(self):
-        out_file = open("out.html" , "w")
         if self.inputExists:
-            out_file.write("Imagen segmentada exitosamente")
-            out_file.close()
-            self.ui_window.textBrowser.setSource('out.html')
-            self.ui_window.textBrowser.reload()
             self.feet_segment()
         else:
             out_file.write("No se ha seleccionado imagen de entrada")
@@ -115,19 +113,13 @@ class Window(QMainWindow):
         return
 
     def temp_extract(self):
-        out_file = open("out.html" , "w")
         if self.inputExists:
-            out_file.write("Se extrajo temperatura exitosamente")
-            out_file.close()
-            self.ui_window.textBrowser.setSource('out.html')
-            self.ui_window.textBrowser.reload()
+            self.messagePrint("Se extrajo la temperatura exitosamente")
         else:
-            out_file.write("No se ha seleccionado imagen de entrada")
-            out_file.close()
-            self.ui_window.textBrowser.setSource('out.html')
-            self.ui_window.textBrowser.reload()
+            self.messagePrint("No se ha seleccionado un imagen de entrada")
     
     def figlabels(self):
+        #  Get info from directory path name and obtain time indexes based on name
         pass
 
     def openImage(self):
@@ -160,19 +152,22 @@ class Window(QMainWindow):
             self.findImages()
 
     def makeTimePlot(self):
-        x=np.array([0,1,5,10,15,20])
-        y=np.array([35.5, 35.7 , 36 , 37.2 , 37.3, 37.5])
-        fig=plt.figure(figsize=(9.6,4))
-        plt.plot(x,y,label='Paciente 1')
-        plt.legend()
-        plt.grid()
-        plt.xlabel("Tiempo [minutos]")
-        plt.ylabel("Temperatura [°C]")
-        plt.title("Time plot")
-        #plt.show()
-        plt.savefig('fresh.png')
-        self.ui_window.timePlot.setPixmap('fresh.png')
-
+        if self.inputExists:
+            x=np.array([0,1,5,10,15,20])
+            y=np.array([35.5, 35.7 , 36 , 37.2 , 37.3, 37.5])
+            fig=plt.figure(figsize=(9.6,4))
+            plt.plot(x,y,label='Paciente 1')
+            plt.legend()
+            plt.grid()
+            plt.xlabel("Tiempo [minutos]")
+            plt.ylabel("Temperatura [°C]")
+            plt.title("Time plot")
+            #plt.show()
+            plt.savefig('fresh.png')
+            self.ui_window.timePlot.setPixmap('fresh.png')
+            self.messagePrint("Se ha actualizado el TimePlot")
+        else:
+            self.messagePrint("No se puede actualizar el TimePlot. No se ha seleccionado una imagen de entrada")
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
