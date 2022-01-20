db3418765f7c244121ab96d2fb8cc3463753f76b
blotero <blotero@gmail.com>
2022-01-19 21:57:56 -0500

    Change in install script

--- install.sh
+++ install.sh
@@ -4,12 +4,14 @@ path=$(pwd)
 install_path=/usr/bin/feetgui
 
 echo "Using path " $path " as source path"
+pip install -r requirements.txt
 echo "Installing executable into " $install_path " ..."
 touch $install_path
 echo "#!/bin/sh" > $install_path
 echo "cd " $path >> $install_path
 echo "python main.py" >> $install_path
 ln $install_path ~/Desktop/
+ln $install_path /usr/local/bin
 chmod +x $install_path
 
 echo "Succesfully installed FEET-GUI"
